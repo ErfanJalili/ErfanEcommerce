ab3a93f0395d877e615a5b4585c48ec04adc87ed
seyed erfan jalili <41292217+ErfanJalili@users.noreply.github.com>
2024-02-11 19:11:22 +0330

    config cqrs

--- src/BuildingBlocks/CQRS/Extensions.cs
+++ src/BuildingBlocks/CQRS/Extensions.cs
@@ -5,6 +5,7 @@ using BuildingBlocks.CQRS.Queris;
 using BuildingBlocks.CQRS.Queris.Dispatcher;
 using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using System.Windows.Input;
@@ -13,17 +14,25 @@ namespace BuildingBlocks.CQRS
 {
     public static class Extensions
     {
-        public static IServiceCollection AddCqrs(this IServiceCollection services)
+        public static IServiceCollection AddCqrs(this IServiceCollection services, Assembly assembly)
         {
-            services.AddMediatR(c =>
+            services.AddMediatR(config =>
             {
-                c.RegisterServicesFromAssemblies(typeof(ICommand<>).Assembly);
-                c.AddOpenBehavior(typeof(ValidationBehavior<,>));
-                c.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                config.RegisterServicesFromAssembly(assembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
-            services.AddValidatorsFromAssembly(typeof(ICommand<>).Assembly);
-            services.AddScoped<ICommandDispatcher, CommandDispatcher>();
-            services.AddScoped<IQueryDispatcher, QueryDispatcher>();
+            services.AddValidatorsFromAssembly(assembly);
+
+            //services.AddMediatR(c =>
+            //{
+            //    c.RegisterServicesFromAssemblies(typeof(ICommand<>).Assembly);
+            //    c.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            //    c.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            //});
+            //services.AddValidatorsFromAssembly(typeof(ICommand<>).Assembly);
+            //services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+            //services.AddScoped<IQueryDispatcher, QueryDispatcher>();
             return services;
         }
     }
--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using BuildingBlocks.CQRS;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -10,15 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add services to the container.
-var assembly = typeof(Program).Assembly;
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
-builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCqrs(typeof(Program).Assembly);
 
 builder.Services.AddCarter();
 
